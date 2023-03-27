db223e6188175c818b5b20dd518b935c095cb0c9
Ethan <116187182+Damadog101@users.noreply.github.com>
2023-03-26 19:07:47 -0700

    base stuff done

---
+++ script.js
@@ -0,0 +1,31 @@
+let base = document.getElementById("base");
+let alertOne = document.getElementById("alertOne");
+let alertTwo = document.getElementById("alertTwo");
+let alertThree = document.getElementById("alertThree");
+let yes = document.getElementById("yes");
+let no = document.getElementById("no");
+let exit = document.getElementById("exit");
+
+base.addEventListener("click", () => {
+	alertOne.classList.remove("noDisplay");
+});
+yes.addEventListener("click", () => {
+	alertOne.classList.add("noDisplay");
+	alertTwo.classList.remove("noDisplay");
+});
+exit.addEventListener("click", () => {
+	alertTwo.classList.add("noDisplay");
+});
+
+no.addEventListener("click", () => {
+	alertTwo.classList.add("noDisplay");
+	alertThree.classList.remove("noDisplay");
+});
+// yes.addEventListener("click", () => {
+// 	alertOne.classList.add("noDisplay");
+// 	alertTwo.classList.remove("noDisplay");
+// });
+// yes.addEventListener("click", () => {
+// 	alertOne.classList.add("noDisplay");
+// 	alertTwo.classList.remove("noDisplay");
+// });
